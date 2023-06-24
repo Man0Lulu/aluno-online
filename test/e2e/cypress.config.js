c26b5529449f3d52974e599cd7d7d27347ec4684
unknown <luisrocksousa1111@gmail.com>
2023-06-23 22:14:40 -0300

    corrigindo cypress

--- test/e2e/cypress.config.js
+++ test/e2e/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {},
     specPattern: 'tests/**/*.cy.{js,jsx,ts,tsx}',
   },
--- test/e2e/tests/requerimento.cy.js
+++ test/e2e/tests/requerimento.cy.js
@@ -10,7 +10,7 @@ describe('Testando',()=>{
                 .select("ADS032 - Construção de Frontend")
        cy.get("#inputProva")
                 .select("A1")
-       cy.get('#inputArgumentacao').type('Teste Cypress')
+       cy.get('#inputArgumentacao').type('Luizão')
        cy.get('body > div > div > div.card-body > form > button').click()
        cy.get('body > div > div > div.card-body').contains('ADS032 - Construção de Frontend')
        cy.get('body > div > div > div.card-body').contains('Pendente')
